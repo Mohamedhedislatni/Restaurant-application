d2c2a7f656093b1797e4c6bef2fe304f4b750d85
Mohamed Hedi Slatni <medhedislatni@gmail.com>
2021-08-07 18:45:20 +0100

    first commit

---
+++ client/src/components/mainpages/contactus/contactus.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Send from './Send'
+
+
+
+
+
+const Contactus = () => {
+    return (
+        <div>
+        <Send/>
+        </div>
+    )
+}
+
+export default Contactus
